d3a8ae3100c023587d433f0f6525cb58fc8c5b92
lucky <joeaneto@gmail.com>
2024-04-28 18:53:53 -0300

    relogio final

---
+++ js/visao.js
@@ -0,0 +1,15 @@
+const horas = window.document.getElementById('horas');
+const minutos = window.document.getElementById('minutos');
+const segundos = window.document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    horas.textContent = hr;
+    minutos.textContent=min;
+    segundos.textContent=s; 
+})
